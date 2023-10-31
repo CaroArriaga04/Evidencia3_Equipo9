6cb6af6cd135428ad3bc1b9d2235d6788580ea94
Carolina Arriaga Galaviz <c.arriagag03@gmail.com>
2023-10-31 17:31:57 -0600

    Creacion de tablas

---
+++ Base_de_datos.py
@@ -0,0 +1,24 @@
+import sys
+import sqlite3
+from sqlite3 import Error
+
+try:
+    with sqlite3.connect("TallerMecanico.db") as conn:
+        mi_cursor = conn.cursor()
+        mi_cursor.execute("CREATE TABLE IF NOT EXISTS Cliente\
+                          (claveCliente INTEGER PRIMARY KEY, nombre TEXT NOT NULL, rfc TEXT NOT NULL, correo TEXT NOT NULL);")
+        mi_cursor.execute("CREATE TABLE IF NOT EXISTS Nota\
+                          (folio INTEGER PRIMARY KEY, fecha timestamp NOT NULL, monto REAL, claveCliente INTEGER NOT NULL,\
+                           FOREIGN KEY(claveCliente) REFERENCES Cliente(claveCliente));")
+        mi_cursor.execute("CREATE TABLE IF NOT EXISTS Servicio\
+                          (claveServicio INTEGER PRIMARY KEY, nombre TEXT NOT NULL, costo REAL);")
+        mi_cursor.execute("CREATE TABLE IF NOT EXISTS Detalle\
+                          (claveServicio INTEGER, folio INTEGER,\
+                          FOREIGN KEY(claveServicio) REFERENCES Servicio(claveServicio),\
+                          FOREIGN KEY(folio) REFERENCES Nota(folio));")
+except Error as e:
+    print (e)
+except Exception:
+    print(f"Se produjo el siguiente error: {sys.exc_info()[0]}")
+finally:
+    conn.close()
